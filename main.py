eed8436936432a4bbcd2817703de7e3ace76739b
MuriloAltoe <muriloaleme17@gmail.com>
2023-03-01 23:58:40 -0300

    another_file_add

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 
-
+import items
 
 
 main = Tk(className='Cadastrar festa')
@@ -19,16 +19,20 @@ def open_cad_festa():
 def open_cad_cliente():
   cad_cliente = Tk(className='Cadastrar Cliente')
   cad_cliente.geometry("700x400")
-  ttk.Label(cad_cliente, text='Nome do cliente:')
-  ttk.Entry
-  ttk.Button(cad_cliente, text='close').pack(pady= 20)
+  ttk.Label(cad_cliente, text='Nome do cliente:').grid(column = 3, row = 1)
+
+  name = ""
+  ttk.Entry(cad_cliente, width = 25, textvariable = name).grid(column = 3, row = 2)
+
+  ttk.Combobox(cad_cliente, values=items.meses).grid(column = 4, row = 2)
+
 
 
 
 
 
 ttk.Button(main, text= "Cadastrar festa", command=open_cad_festa).pack(pady= 20)
-ttk.Button(main, text= "Cadastrar Cliente", command=open_cad_festa).pack(pady= 20)
+ttk.Button(main, text= "Cadastrar Cliente", command=open_cad_cliente).pack(pady= 20)
 ttk.Button(main, text= "Sair", command=lambda: main.quit()).pack(pady= 20)
 
 
